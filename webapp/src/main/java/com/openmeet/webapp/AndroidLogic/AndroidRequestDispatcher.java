3e170f4439c271d770e567cfad94ac5081252966
Yuri Brandi <destroyercrazy0@gmail.com>
2023-01-07 21:22:14 +0100

    jdbc android demo

---
+++ webapp/src/main/java/com/openmeet/webapp/AndroidLogic/AndroidRequestDispatcher.java
@@ -0,0 +1,27 @@
+package com.openmeet.webapp.AndroidLogic;
+
+import com.google.gson.Gson;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AndroidRequestDispatcher extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        PrintWriter out = resp.getWriter();
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        System.out.println("request received");
+
+        out.println("success" + email + " | " + password);
+
+    }
+}
