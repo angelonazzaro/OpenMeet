db137ba1ca7df9452b9b3ccddcda3ccc4e52b7bd
Francescoo45 <granoziofrancesco.1@outlook.it>
2023-01-11 17:02:36 +0100

    commented multiMapList.java

--- shared/src/main/java/com/openmeet/shared/data/storage/DAO.java
+++ shared/src/main/java/com/openmeet/shared/data/storage/DAO.java
@@ -127,6 +127,20 @@ public interface DAO<T> {
      */
     boolean doDelete(String condition) throws SQLException;
 
+    /**
+     * Returns a list of objects from the database that match a given condition.
+     *
+     * @param table the name of the table in the database from which the records are to be retrieved.
+     * @param condition the condition that the records should match in order to be retrieved.
+     * @param extractor an instance of a class that implements the ResultSetExtractor interface.
+     * The extractor is used to convert the ResultSet returned by the database query into a
+     * List of objects of the desired type.
+     * @param source an instance of DataSource, an object used to establish a connection to the database.
+     * @return the query results as a list of objects.
+     *
+     * @author Francesco Granozio
+     * @author Angelo Nazzaro
+     */
     default <E extends ResultSetExtractor<T>> List<T> genericDoRetrieveByCondition(String table, String condition, E extractor, DataSource source) throws SQLException {
 
         final List<T> entity = new ArrayList<>();
@@ -148,6 +162,18 @@ public interface DAO<T> {
         return entity;
     }
 
+    /**
+     * Saves an object in the database.
+     *
+     * @param table the name of the table in the database from which the records are to be retrieved.
+     * @param map a HashMap representing the data to be saved, where the keys are the column
+     * names and the values are the values to be inserted.
+     * @param source an instance of DataSource, an object used to establish a connection to the database.
+     * @return a boolean value that indicates if the operation was successful or not.
+     *
+     * @author Francesco Granozio
+     * @author Angelo Nazzaro
+     */
     default boolean genericDoSave(String table, HashMap<String, ?> map, DataSource source) throws SQLException {
 
         int rows;
@@ -165,6 +191,19 @@ public interface DAO<T> {
         return rows == 1;
     }
 
+    /**
+     * Updates an object in the database.
+     *
+     * @param table the name of the table in the database from which the records are to be retrieved.
+     * @param condition the condition that the records should match in order to be retrieved.
+     * @param values a HashMap representing the data to be updated, where the keys are the
+     * column names and the values are the new values to be set.
+     * @param source an instance of DataSource, an object used to establish a connection to the database.
+     * @return a boolean value that indicates if the operation was successful or not.
+     *
+     * @author Francesco Granozio
+     * @author Angelo Nazzaro
+     */
     default boolean genericDoUpdate(String table, String condition, HashMap<String, ?> values, DataSource source) throws SQLException {
 
         int rows;
@@ -182,6 +221,17 @@ public interface DAO<T> {
         return rows > 0;
     }
 
+    /**
+     * Deletes an object in the database.
+     *
+     * @param table the name of the table in the database from which the records are to be retrieved.
+     * @param condition the condition that the records should match in order to be retrieved.
+     * @param source an instance of DataSource, an object used to establish a connection to the database.
+     * @return a boolean value that indicates if the operation was successful or not.
+     *
+     * @author Francesco Granozio
+     * @author Angelo Nazzaro
+     */
     default boolean genericDoDelete(String table, String condition, DataSource source) throws SQLException {
 
         int rows;
