ed782a5f10c707a8684f6c17158df392b5a33e61
robertodr01 <roberto@dellarocca.org>
2023-01-24 00:13:03 +0100

    Creating Meeter_InterestService.java

---
+++ webservice/src/main/java/com/openmeet/webservice/Meeter_InterestService.java
@@ -0,0 +1,151 @@
+package com.openmeet.webservice;
+
+import com.openmeet.shared.data.meeter_interest.Meeter_Interest;
+import com.openmeet.shared.data.meeter_interest.Meeter_InterestDAO;
+import com.openmeet.shared.data.storage.DAO;
+import com.openmeet.shared.helpers.ResponseHelper;
+import com.openmeet.webservice.proxies.Meeter_InterestProxyDAO;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Meeter_InterestService extends HttpServlet {
+
+    private static final Logger logger = Logger.getLogger(LoginServlet.class.getName());
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String operation = request.getParameter("operation");
+        PrintWriter out = response.getWriter();
+
+        //check if all the parameters are present
+        if (!ResponseHelper.checkStringFields(operation)) {
+            ResponseHelper.sendGenericError(out);
+            logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: missing parameters - operation");
+            return;
+        }
+
+        Meeter_InterestDAO meeter_interestDAO = new Meeter_InterestDAO((DataSource) getServletContext().getAttribute("DataSource"));
+        Meeter_InterestProxyDAO meeter_interestProxyDAO = new Meeter_InterestProxyDAO(meeter_interestDAO, request, out);
+
+        switch (operation) {
+            case DAO.DO_RETRIEVE_BY_CONDITION: {
+                try {
+
+                    meeter_interestProxyDAO.doRetrieveByCondition(null);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_RETRIEVE_BY_KEY: {
+
+                try {
+                    meeter_interestProxyDAO.doRetrieveByKey(null);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_RETRIEVE_ALL: {
+                try {
+                    meeter_interestProxyDAO.doRetrieveAll();
+                } catch (SQLException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_RETRIEVE_ALL_LIMIT: {
+                try {
+                    meeter_interestProxyDAO.doRetrieveAll(0);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_RETRIEVE_ALL_LIMIT_OFFSET: {
+                try {
+                    meeter_interestProxyDAO.doRetrieveAll(0, 0);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_SAVE: {
+                try {
+                    meeter_interestProxyDAO.doSave(new Meeter_Interest());
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_SAVE_PARTIAL:
+                E:
+                {
+                    try {
+                        meeter_interestProxyDAO.doSave(new HashMap<>());
+                    } catch (SQLException | InvalidParameterException e) {
+                        ResponseHelper.sendGenericError(out);
+                        logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                        return;
+                    }
+                    break;
+                }
+            case DAO.DO_UPDATE: {
+                try {
+                    meeter_interestProxyDAO.doUpdate(null, null);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_SAVE_OR_UPDATE: {
+                try {
+                    meeter_interestProxyDAO.doSaveOrUpdate(null);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            case DAO.DO_DELETE: {
+                try {
+                    meeter_interestProxyDAO.doDelete(null);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
+            default:
+                ResponseHelper.sendGenericError(out);
+                logger.log(Level.SEVERE, "Meeter_InterestService:doPost() - Error: unknown operation");
+                break;
+        }
+    }
+
+}
