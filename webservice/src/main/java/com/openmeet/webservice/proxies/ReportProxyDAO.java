3dec0c50d8679b3386555e5080d1b6d01db9cc56
robertodr01 <roberto@dellarocca.org>
2023-01-23 23:31:10 +0100

    Creating ReportProxyDAO.java

---
+++ webservice/src/main/java/com/openmeet/webservice/proxies/ReportProxyDAO.java
@@ -0,0 +1,203 @@
+package com.openmeet.webservice.proxies;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.openmeet.shared.data.meeter.Meeter;
+import com.openmeet.shared.data.report.Report;
+import com.openmeet.shared.data.storage.DAO;
+import com.openmeet.shared.data.storage.GenericProxyDAO;
+import com.openmeet.shared.exceptions.InvalidPrimaryKeyException;
+import com.openmeet.shared.helpers.ResponseHelper;
+import com.openmeet.webservice.InvalidParameterException;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+
+public class ReportProxyDAO extends ProxyDAO<Report> implements DAO<Report> {
+
+
+    public ReportProxyDAO(DAO<Report> dao, HttpServletRequest request, PrintWriter out) {
+        super(dao, request, out);
+    }
+
+    @Override
+    public List<Report> doRetrieveByCondition(String condition) throws SQLException {
+
+        condition = request.getParameter("condition");
+
+        if (!ResponseHelper.checkStringFields(condition)) {
+            throw new java.security.InvalidParameterException("Missing parameters - condition");
+        }
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveByCondition() - condition: " + condition);
+
+        List<Report> reports = GenericProxyDAO.genericProxyDoRetrieveByCondition(condition, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveByCondition() - reports: " + reports);
+
+        return reports;
+
+    }
+
+    @Override
+    public Report doRetrieveByKey(String key) throws SQLException, InvalidPrimaryKeyException {
+
+        key = request.getParameter("key");
+
+        if (!ResponseHelper.checkStringFields(key)) {
+            throw new java.security.InvalidParameterException("Missing parameters - key");
+        }
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveByKey() - key: " + key);
+
+        Report report = GenericProxyDAO.genericProxyDoRetrieveByKey(key, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveByKey() - meeter: " + report);
+
+        return report;
+
+    }
+
+    @Override
+    public List<Report> doRetrieveAll() throws SQLException {
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveAll()");
+
+        List<Report> reports = GenericProxyDAO.genericProxyDoRetrieveByCondition("TRUE", dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveAll() - reports: " + reports);
+
+        return reports;
+
+    }
+
+    @Override
+    public List<Report> doRetrieveAll(int row_count) throws SQLException {
+
+        row_count = Integer.parseInt(request.getParameter("row_count"));
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveAll() - row_count: " + row_count);
+
+        if (row_count <= 0) {
+            throw new InvalidParameterException("Invalid parameters - row count");
+        }
+
+        List<Report> reports = GenericProxyDAO.genericProxyDoRetrieveByCondition("TRUE LIMIT " + row_count, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveAll() - reports: " + reports);
+
+        return reports;
+
+    }
+
+    @Override
+    public List<Report> doRetrieveAll(int offset, int row_count) throws SQLException {
+
+        row_count = Integer.parseInt(request.getParameter("row_count"));
+        offset = Integer.parseInt(request.getParameter("offset"));
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveAll() - offset: " + offset + ", row_count: " + row_count);
+
+        if (row_count <= 0) {
+            throw new InvalidParameterException("Invalid parameters - row count");
+        }
+        if (offset < 0) {
+            throw new InvalidParameterException("Invalid parameters - offset");
+        }
+
+        List<Report> reports = GenericProxyDAO.genericProxyDoRetrieveByCondition("TRUE LIMIT " + offset + ", " + row_count, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doRetrieveAll() - reports: " + reports);
+
+        return reports;
+
+    }
+
+    @Override
+    public boolean doSave(Report obj) throws SQLException {
+
+        Gson builder = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        obj = builder.fromJson(request.getParameter("report"), Report.class);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doSave() - obj: " + obj);
+
+        boolean isSuccessful = GenericProxyDAO.genericProxyDoSave(obj, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doSave() - isSuccessful: " + isSuccessful);
+
+        return isSuccessful;
+
+    }
+
+    @Override
+    public boolean doSave(HashMap<String, ?> values) throws SQLException {
+
+        Gson builder = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        values = builder.fromJson(request.getParameter("values"), HashMap.class);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doSave() - values: " + values);
+
+        boolean isSuccessful = GenericProxyDAO.genericProxyDoSave(values, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doSave() - isSuccessful: " + isSuccessful);
+
+        return isSuccessful;
+
+    }
+
+    @Override
+    public boolean doUpdate(HashMap<String, ?> values, String condition) throws SQLException {
+
+        Gson builder = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        values = builder.fromJson(request.getParameter("values"), HashMap.class);
+        condition = request.getParameter("condition");
+
+        logger.log(Level.INFO, "ReportProxyDAO:doUpdate() - values: " + values + ", condition: " + condition);
+
+        if (!ResponseHelper.checkStringFields(condition)) {
+            throw new InvalidParameterException("Missing parameters - condition");
+        }
+
+        boolean isSuccessful = GenericProxyDAO.genericProxyDoUpdate(values, condition, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doUpdate() - isSuccessful: " + isSuccessful);
+
+        return isSuccessful;
+
+    }
+
+    @Override
+    public boolean doSaveOrUpdate(Report obj) throws SQLException {
+
+        Gson builder = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        obj = builder.fromJson(request.getParameter("report"), Report.class);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doSaveOrUpdate() - obj: " + obj);
+
+        boolean isSuccessful = GenericProxyDAO.genericProxyDoSaveOrUpdate(obj, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doSaveOrUpdate() - isSuccessful: " + isSuccessful);
+
+        return isSuccessful;
+
+    }
+
+    @Override
+    public boolean doDelete(String condition) throws SQLException {
+
+        condition = request.getParameter("condition");
+
+        logger.log(Level.INFO, "ReportProxyDAO:doDelete() - condition: " + condition);
+
+        boolean isSuccessful = GenericProxyDAO.genericProxyDoDelete(condition, dao, out);
+
+        logger.log(Level.INFO, "ReportProxyDAO:doDelete() - isSuccessful: " + isSuccessful);
+
+        return isSuccessful;
+
+    }
+}
