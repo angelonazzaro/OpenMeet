8f1bf2ac506403dc66b2f08de43f5a364168f2ed
robertodr01 <roberto@dellarocca.org>
2023-01-23 20:15:46 +0100

    adding InsertInterests.sql

---
+++ database/InsertInterests.sql
@@ -0,0 +1,37 @@
+USE OpenMeet;
+
+INSERT INTO Interest(description) VALUES
+('Acting'), ('Advertising'), ('Air hockey'), ('American football'), ('Art'),
+ ('Art history'), ('Astronomy'), ('Astrophysics'),
+ ('Badminton'), ('Baking'),('Ballet'),('Ballroom dancing'),('Bar tending'), ('Baseball'),
+ ('Basketball'),('Biking'),('Billiards'), ('Blacksmithing'),('Board games'), ('Boating'),
+ ('Bouldering'), ('Bowling'),('Boxing'),('Calligraphy'),('Card games'), ('Carpentry'),
+ ('Ceramics'),('Choreography'),('Computer programming'), ('Cooking'),
+ ('Cybersecurity'), ('Cycling'),('DJing'), ('Dancing'),('Darts'),('Data analysis'),
+ ('Diving'), ('Drawing'),('Equestrian'), ('Escape rooms'), ('Ethical hacking'),
+ ('Fashion design'), ('Field hockey'),('Filmmaking'),('Filmography'),
+ ('Financial analysis'), ('Fishing'), ('Fitness'),('Floristry'), ('Food criticism'),
+ ('Food styling'),('Football'),('Gaming'),('Gardening'),('Geocaching'),
+ ('Glassblowing'), ('Golf'),('Guitar'),('Gymnastics'),('Hang gliding'), ('Hiking'),
+ ('Horseback riding'),('Hunting'), ('Ice skating'), ('Improv'), ('Interests'),
+ ('Interior design'), ('Investment banking'), ('Jazz'),('Jogging'), ('Journalism'),
+ ('Judo'), ('Jujitsu'),('Karate'),('Kayaking'), ('Kickboxing'),('Kiteboarding'),
+ ('Knitting'), ('Lacrosse'), ('Landscaping'), ('Letterboxing'),('Literature'),
+ ('Marathons'),('Marketing'), ('Martial arts'), ('Mathematics'),('Meditation'),
+ ('Mixology'), ('Mobile app development'),('Mountain biking'), ('Movies'),
+ ('Muay Thai'), ('Music'),('Music production'), ('Nature'), ('Nature Photography'),
+ ('Orienteering'), ('Painting'),('Paragliding'),('Photography'),('Physics'),('Pilates'),
+ ('Pinball'), ('Poetry'),('Polo'),('Pool'),('Pottery'),('Public relations'),
+ ('Puzzle solving'), ('Rappelling'),('Reading'), ('Road cycling'),('Rock Climbing'),
+ ('Rock climbing'), ('Roller hockey'),('Rugby'),('Running'),('Sailing'),('Salsa'),
+ ('Sculpting'), ('Self defense'), ('Sewing'),('Shuffleboard'), ('Singing'),('Skateboarding'),('Sketching'),
+ ('Skiing'),('Skydiving'), ('Snowboarding'), ('Soccer'), ('Squash'),('Stand-up comedy'),
+ ('Stock trading'), ('Surfing'), ('Swimming'), ('Synchronized swimming'),
+ ('Table tennis'),('Teaching'), ('Tennis'),('Theater'), ('Theatre'),
+ ('Travel'),('Treasure hunting'), ('Triathlons'),('Video editing'),('Video games'),
+ ('Volleyball'), ('Water polo'),('Watercolor'), ('Web design'), ('Weightlifting'),
+ ('Windsurfing'),('Wine tasting'),('Woodworking'), ('Wrestling'), ('Writing'),
+ ('Yachting'), ('Yoga'), ('Beer'), ('420');
+ 
+ select * from Interest;
+ 
