4ba7ccc4778f46c930a70387f29da990b1fe4417
Francescoo45 <granoziofrancesco.1@outlook.it>
2023-01-26 17:17:05 +0100

    moved ReportService in services folder

--- webservice/src/main/java/com/openmeet/webservice/services/ReportService.java
+++ webservice/src/main/java/com/openmeet/webservice/services/ReportService.java
@@ -1,9 +1,11 @@
-package com.openmeet.webservice;
+package com.openmeet.webservice.services;
 
 import com.openmeet.shared.data.report.Report;
 import com.openmeet.shared.data.report.ReportDAO;
 import com.openmeet.shared.data.storage.DAO;
 import com.openmeet.shared.helpers.ResponseHelper;
+import com.openmeet.webservice.InvalidParameterException;
+import com.openmeet.webservice.LoginServlet;
 import com.openmeet.webservice.proxies.ReportProxyDAO;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
