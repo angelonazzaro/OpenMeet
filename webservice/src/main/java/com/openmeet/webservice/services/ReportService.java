455dd7ef11a082950774b8a44d70823636eaad2b
Francescoo45 <granoziofrancesco.1@outlook.it>
2023-02-03 12:38:21 +0100

    reportservice added case doRetrieveByConditionLimit

--- webservice/src/main/java/com/openmeet/webservice/services/ReportService.java
+++ webservice/src/main/java/com/openmeet/webservice/services/ReportService.java
@@ -48,12 +48,22 @@ public class ReportService extends HttpServlet {
                 }
                 break;
             }
+            case DAO.DO_RETRIEVE_BY_CONDITION_LIMIT: {
+                try {
+                    reportProxyDAO.doRetrieveByCondition(null, 0);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "ReportService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
             case DAO.DO_RETRIEVE_BY_CONDITION_LIMIT_OFFSET: {
                 try {
                     reportProxyDAO.doRetrieveByCondition(null, 0, 0);
                 } catch (SQLException | InvalidParameterException e) {
                     ResponseHelper.sendGenericError(out);
-                    logger.log(Level.SEVERE, "BanService:doPost() - Error: " + e.getMessage());
+                    logger.log(Level.SEVERE, "ReportService:doPost() - Error: " + e.getMessage());
                     return;
                 }
                 break;
