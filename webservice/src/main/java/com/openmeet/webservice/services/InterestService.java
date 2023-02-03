79c497c4b9f1a4c93094cae8f313ca10c19175b9
Francescoo45 <granoziofrancesco.1@outlook.it>
2023-02-03 12:35:46 +0100

    interestservice added case doRetrieveByConditionLimit

--- webservice/src/main/java/com/openmeet/webservice/services/InterestService.java
+++ webservice/src/main/java/com/openmeet/webservice/services/InterestService.java
@@ -48,12 +48,22 @@ public class InterestService extends HttpServlet {
                 }
                 break;
             }
+            case DAO.DO_RETRIEVE_BY_CONDITION_LIMIT: {
+                try {
+                    interestProxyDAO.doRetrieveByCondition(null, 0);
+                } catch (SQLException | InvalidParameterException e) {
+                    ResponseHelper.sendGenericError(out);
+                    logger.log(Level.SEVERE, "InterestService:doPost() - Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            }
             case DAO.DO_RETRIEVE_BY_CONDITION_LIMIT_OFFSET: {
                 try {
                     interestProxyDAO.doRetrieveByCondition(null, 0, 0);
                 } catch (SQLException | InvalidParameterException e) {
                     ResponseHelper.sendGenericError(out);
-                    logger.log(Level.SEVERE, "BanService:doPost() - Error: " + e.getMessage());
+                    logger.log(Level.SEVERE, "InterestService:doPost() - Error: " + e.getMessage());
                     return;
                 }
                 break;
