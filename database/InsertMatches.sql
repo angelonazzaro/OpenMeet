882a0cfa386617c5d464ccfd3704453bceed3e8f
Yuri Brandi <destroyercrazy0@gmail.com>
2023-02-04 10:16:18 +0100

    insertMatches.sql

---
+++ database/InsertMatches.sql
@@ -0,0 +1,8 @@
+INSERT INTO Rating(meeterRater, meeterRated, type, creationDate) VALUES 
+('2', '10', TRUE, CURDATE()),
+('10', '2', TRUE, CURDATE()),
+('2', '15', TRUE, CURDATE()),
+('15', '2', TRUE, CURDATE()),
+('2', '22', TRUE, CURDATE()),
+('22', '2', TRUE, CURDATE()),
+('2', '25', TRUE, CURDATE());
