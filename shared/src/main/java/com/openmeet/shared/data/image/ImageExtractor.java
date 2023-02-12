bd6796f9749724c1f377befb43d5c690743baef6
sl1mSha4dey <mutenssj@gmail.com>
2023-02-12 12:48:26 +0100

    add Image bean javadoc

--- shared/src/main/java/com/openmeet/shared/data/image/ImageExtractor.java
+++ shared/src/main/java/com/openmeet/shared/data/image/ImageExtractor.java
@@ -5,7 +5,22 @@ import com.openmeet.shared.data.storage.ResultSetExtractor;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * The extractor is used to convert the ResultSet returned by the database query into an Image object.
+ *
+ * @author Angelo Nazzaro
+ * @author Francesco Granozio
+ */
 public class ImageExtractor implements ResultSetExtractor<Image> {
+    /**
+     * Returns an Image object from the result set.
+     *
+     * @param resultSet the result set deriving from the query statement.
+     * @return the Image object contained in the resultSet.
+     *
+     * @author Francesco Granozio
+     * @author Angelo Nazzaro
+     */
     @Override
     public Image extract(ResultSet resultSet) throws SQLException {
 
