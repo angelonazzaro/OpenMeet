0521c25cb6c1ef170108d6ec0cc2b5b60d88c2b3
robertodr01 <roberto@dellarocca.org>
2023-02-16 19:15:38 +0100

    added doc in InvalidParameterException.java

--- webservice/src/main/java/com/openmeet/webservice/exceptions/InvalidParameterException.java
+++ webservice/src/main/java/com/openmeet/webservice/exceptions/InvalidParameterException.java
@@ -1,6 +1,12 @@
 package com.openmeet.webservice.exceptions;
 
+/**
+ *  Exception for invalid paramaters sended from requests
+ */
 public class InvalidParameterException extends RuntimeException {
+    /**
+     * @param s describe exception
+     */
     public InvalidParameterException(String s) {
         super(s);
     }
