fe163841ce5572e7b18652aa955abc02a799a80b
Angelo Nazzaro <58223071+sl1mSha4dey@users.noreply.github.com>
2023-02-16 19:54:18 +0100

    Update Meeter_InterestService.java

--- webservice/src/main/java/com/openmeet/webservice/services/Meeter_InterestService.java
+++ webservice/src/main/java/com/openmeet/webservice/services/Meeter_InterestService.java
@@ -21,7 +21,7 @@ import java.util.logging.Logger;
 /**
  * Servlet that handles all invocations for the Meeter_Interest object.
  *
- * @author robertodellarocca
+ * @author Roberto Della Rocca
  */
 public class Meeter_InterestService extends HttpServlet {
 
@@ -31,6 +31,8 @@ public class Meeter_InterestService extends HttpServlet {
      * @param request  an {@link HttpServletRequest} object that contains the request the client has made of the servlet. In specific contain "operation" parameter that contains the name of operation requested and invokes the corresponding proxy method.
      * @param response an {@link HttpServletResponse} object that contains the response the servlet sends to the client
      * @throws IOException
+     * 
+     * @author Roberto Della Rocca
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
